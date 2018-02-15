370f4d16831005e3a9db9ceed8f0b2e03ec177a7
jvelezpo <sebasvelez@gmail.com>
2018-02-15 17:40:24 -0500

    worker implementation to save status on db

--- receive.js
+++ receive.js
@@ -1,4 +1,18 @@
 const amqp = require('amqplib/callback_api');
+const request = require('request');
+const bookmarks = require('./lib/db/bookmarks');
+
+const updateStatus = function(id, status) {
+  const params = {
+    id: id,
+    is_ok: status
+  }
+  bookmarks.updateStatus(params, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
@@ -7,7 +21,16 @@ amqp.connect('amqp://localhost', function(err, conn) {
     ch.assertQueue(q, {durable: false});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
+
+      msg = JSON.parse(msg.content);
+      request(msg.url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          updateStatus(msg.id, true);
+        } else {
+          updateStatus(msg.id, false);
+        }
+        console.log(`url ${msg.url} checked`);
+      })
     }, {noAck: true});
   });
 });
